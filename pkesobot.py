6be39b6bdba5cea40e911b54105e15a23c20fc8f
siengsotheara <siengsotheara.info@gmail.com>
2017-05-08 21:42:40 +0700

    more

--- pkesobot.py
+++ pkesobot.py
@@ -26,9 +26,11 @@ def handle_message():
                 if messaging_event.get("message"):
                     sender_id = messaging_event["sender"]["id"]
                     recipient_id = messaging_event["recipient"]["id"]
-                    message_text = messaging_event["message"]["text"]
-
+                    message_text = ""
+                    if messaging_event["message"]["text"]:
+                        message_text = messaging_event["message"]["text"]
                     messagel = message_text.tolower()
+
                     if messagel== "hi" or messagel == "hello":
                         send_message(sender_id, "Halo,. How can I help you ?")
                     else:
@@ -60,6 +62,9 @@ def send_message(recipient_id, message_text):
         },
         "message": {
             "text": message_text
+        }
+        "quick_reply": {
+            "payload":"DEVELOPER_DEFINED_PAYLOAD"
         }
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
