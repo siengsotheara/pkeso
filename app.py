ddfda6a46d25941b1202150038a5d8d8c4ea6ecc
siengsotheara <siengsotheara.info@gmail.com>
2017-05-08 13:13:38 +0700

    asd

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +12,5 @@ def login():
 	return '<h3>this is login page.</h3>'
 
 if __name__ == '__main__':
-	app.run()
+	port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
