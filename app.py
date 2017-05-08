94aac04010f9010d8c3a981651c0170cb449794b
siengsotheara <siengsotheara.info@gmail.com>
2017-05-08 10:16:14 +0700

    add source file

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return '<h1>Flask going to be deployed.</h1>'
+	
+@app.route('/logoin')
+def login():
+	return 'this is login page.'
+
+if __name__ == '__main__':
+	app.run()
