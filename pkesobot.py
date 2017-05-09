0f9cb94c2d6af70fccdfd42e0cf10da8ffa9d073
siengsotheara <siengsotheara.info@gmail.com>
2017-05-09 14:21:08 +0700

    fb not allowed to send mutlti sender_action

--- pkesobot.py
+++ pkesobot.py
@@ -47,7 +47,7 @@ def send_message(recipient_id, message_text):
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
     params = {
-        "access_token": ACCESS_TOKEN
+        "access_token": PAGE_ACCESS_TOKEN
     }
     headers = {
         "Content-Type": "application/json"
@@ -58,9 +58,15 @@ def send_message(recipient_id, message_text):
         },
         "message": {
             "text": message_text
-        },
-        "sender_action": "typing_on"
+        }
     })
+    data1 = json.dumps({
+        "recipient": {
+            "id":recipient_id 
+        },
+        "sender_action":"typing_on"
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data1)
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
